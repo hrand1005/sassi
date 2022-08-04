c6451b027fa5b2350bfe882f5735fdde1e6e4c58
Herbie <hjrand1005@gmail.com>
2022-08-04 08:55:18 -0400

    ADD: uniqueness constraints for new users

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 from models.db import db 
+from typing import Type
 
 class User(db.Document):
     user_id = db.IntField()
     name = db.StringField(required=True)
     password = db.StringField(required=True)
-    email = db.StringField()
-
+    email = db.StringField(unique=True, required=True)
